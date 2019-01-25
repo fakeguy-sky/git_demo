f2ffb80d7f87a8cbc138f523b048243116aebf4b
fakeguy <524086616@qq.com>
2019-01-25 10:35:10 +0800

    init1.0

---
+++ src/main/java/com/fakeguy/git/demo/GitDemo.java
@@ -0,0 +1,14 @@
+package com.fakeguy.git.demo;
+
+/**
+ * @description：
+ * @author： hzc
+ * @date： 2019-01-25 10:23
+ * @version： 1.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+        System.out.println("hehe!");
+    }
+}
